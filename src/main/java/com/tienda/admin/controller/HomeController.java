720da373879fe7c7c7f0f7b7cd120640c5306f84
chenaoh <cdhenao@gmail.com>
2026-01-12 17:08:33 +0100

    Agregando controlador para index.html

---
+++ src/main/java/com/tienda/admin/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.tienda.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "forward:/index.html";
+    }
+    
+    // Esto manejará todas las rutas de tu SPA (Single Page Application)
+    @GetMapping("/{path:[^\\.]*}")
+    public String redirectToIndex() {
+        return "forward:/index.html";
+    }
+}
